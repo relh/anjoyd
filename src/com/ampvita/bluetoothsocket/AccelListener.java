e4d8b276f07f185612918d8b82d50eb2a4eb9ba3
Richard Higgins <relh@terpmail.umd.edu>
2013-10-05 19:01:13 -0400

    working

---
+++ src/com/ampvita/bluetoothsocket/AccelListener.java
@@ -0,0 +1,76 @@
+/*
+    GlassHUD - Heads Up Display for Google Glass
+    Copyright (C) 2013 James Betker
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ampvita.bluetoothsocket;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class AccelListener extends Service implements SensorEventListener{
+	//States
+	SensorManager sensorManager;
+	Sensor aSensor;
+	
+	@Override
+	public void onCreate(){
+		super.onCreate();
+
+		//Then sensors
+		sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+		aSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorManager.registerListener(this, aSensor, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+		
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float[] values = event.values;
+		
+		Intent intent = new Intent(SendingdataActivity.SENSOR_UPDATE_INTENT);
+		intent.putExtra("x", values[0]);
+		intent.putExtra("y", values[1]);
+		intent.putExtra("z", values[2]);
+		
+		sendBroadcast(intent);
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return new Binder();
+	}
+	
+	float val1 = 0f;
+	float val2 = 0f;
+	float val3 = 0f;
+	
+	public void gyroReading(float[] values){
+		val1 = values[0];
+		val2 = values[1];
+		val3 = values[2];
+	}
+}
---
+++ src/com/ampvita/bluetoothsocket/SendingdataActivity.java
@@ -0,0 +1,156 @@
+package com.ampvita.bluetoothsocket;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SendingdataActivity extends Activity {
+	/** Called when the activity is first created. */
+	private BluetoothAdapter mBluetoothAdapter = null;
+	static final UUID MY_UUID = UUID
+			.fromString("4e4610be-2d72-11e3-a59d-f23c91aec05e");
+	static String address = "00:1B:DC:06:62:48";
+
+	static SendData sendData;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			Toast.makeText(this, "Bluetooth is not available.",
+					Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		}
+		if (!mBluetoothAdapter.isEnabled()) {
+			Toast.makeText(this,
+					"Please enable your BT and re-run this program.",
+					Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		} // have correct adapter
+		sendData = new SendData();
+		sendData.sendMessage("hey dan");
+	}
+
+	private boolean mReceiversRegistered = false;
+
+	static String SENSOR_UPDATE_INTENT = "com.ampvita.bluetoothsocket.SENSOR_UPDATE_INTENT";
+
+	// Define a handler and a broadcast receiver
+	private final Handler mHandler = new Handler();
+	private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+	    
+		public float normalize(float in) {
+			return (float)(Math.floor(((in/9.8)*255)));
+		}
+		
+		@Override
+	    public void onReceive(Context context, Intent intent) {
+	        if (intent.getAction().equals(SENSOR_UPDATE_INTENT)) {
+	        	
+	        	float x = intent.getExtras().getFloat("x");
+	        	float y = intent.getExtras().getFloat("y");
+	        	float z = intent.getExtras().getFloat("z");
+	        	
+	        	x = normalize(x);
+	        	y = normalize(y);
+	        	z = normalize(z);
+	        	
+	        	TextView snes = (TextView) findViewById(R.id.textView1);
+	        	snes.setText("x:"+x+"\ny:"+y+"\nz:"+z);
+	        	
+	        	sendData.sendMessage(x + " " + y + " " + z);
+	    	}
+	    }
+	};
+	
+	@Override
+	public void onStart(){
+		super.onStart();
+		startService(new Intent(this, AccelListener.class));
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		IntentFilter intentToReceiveFilter = new IntentFilter();
+		intentToReceiveFilter.addAction(SENSOR_UPDATE_INTENT);
+		this.registerReceiver(mIntentReceiver, intentToReceiveFilter, null, mHandler);
+		mReceiversRegistered = true;
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		  // Make sure you unregister your receivers when you pause your activity
+		  if(mReceiversRegistered) {
+		    unregisterReceiver(mIntentReceiver);
+		    mReceiversRegistered = false;
+		  }
+	}
+	
+	class SendData extends Thread {
+		private BluetoothDevice device = null;
+		private BluetoothSocket btSocket = null;
+		private OutputStream outStream = null;
+		
+		@SuppressLint("NewApi") public SendData() {
+			device = mBluetoothAdapter.getRemoteDevice(address);
+			try {
+				btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+			} catch (Exception e) {
+				Log.e("connect", "no socket");
+			}
+			mBluetoothAdapter.cancelDiscovery();
+			try {
+				btSocket.connect();
+			} catch (IOException e) {
+				Log.e("connect", "no connect");
+				e.printStackTrace();
+				try {
+					btSocket.close();
+				} catch (IOException e2) {
+					Log.e("connect", "no close");
+				}
+			}
+			Toast.makeText(getBaseContext(),
+					"Connected to " + device.getName(), Toast.LENGTH_SHORT)
+					.show();
+			try {
+				outStream = btSocket.getOutputStream();
+			} catch (IOException e) {
+				Log.e("connect", "no stream");
+			}
+		}
+
+		public void sendMessage(String data) {
+			try {
+				mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+				byte[] b = data.getBytes();
+				outStream.write(b);
+				outStream.flush();
+			} catch (IOException e) {
+			}
+		}
+	}
+}
